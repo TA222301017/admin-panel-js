a1207f5fb1944ca9e1c45df54138b9379244f41c
Elkhan Julian Brillianshah <ubbeg2000@gmail.com>
2023-01-23 06:12:29 +0700

    add: added code splitting

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,25 +1,23 @@
-import React, { useState, useMemo } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import AccessLogs from "./pages/AccessLogs";
-import KeyPosition from "./pages/KeyPosition";
-import Locks from "./pages/Locks";
-import EditLock from "./pages/EditLock";
-import Personels from "./pages/Personels";
-import Login from "./pages/Login";
-import AddPersonel from "./pages/AddPersonel";
-import EditPersonel from "./pages/EditPersonel";
-import AddKey from "./pages/AddKey";
-import EditKey from "./pages/EditKey";
-import Keys from "./pages/Keys";
-import HealthcheckLogs from "./pages/HealthcheckLogs";
+import React, { useMemo, lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useSelector } from "react-redux";
+
+import LoaderCover from "./components/LoaderCover";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const AccessLogs = lazy(() => import("./pages/AccessLogs"));
+const KeyPosition = lazy(() => import("./pages/KeyPosition"));
+const Locks = lazy(() => import("./pages/Locks"));
+const EditLock = lazy(() => import("./pages/EditLock"));
+const Personels = lazy(() => import("./pages/Personels"));
+const Login = lazy(() => import("./pages/Login"));
+const AddPersonel = lazy(() => import("./pages/AddPersonel"));
+const EditPersonel = lazy(() => import("./pages/EditPersonel"));
+const AddKey = lazy(() => import("./pages/AddKey"));
+const EditKey = lazy(() => import("./pages/EditKey"));
+const Keys = lazy(() => import("./pages/Keys"));
+const HealthcheckLogs = lazy(() => import("./pages/HealthcheckLogs"));
 
 const AppRouter = () => {
   const mode = useSelector((state) => state.color.value);
@@ -37,27 +35,36 @@ const AppRouter = () => {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Routes>
-          <Route path="/" exact element={<Login />} />
-        </Routes>
+        <Suspense fallback={<LoaderCover show={true} />}>
+          <Routes>
+            <Route path="/" exact element={<Login />} />
+          </Routes>
 
-        <Routes>
-          <Route path="/dashboard" exact element={<Dashboard />} />
-          <Route path="/access-log" exact element={<AccessLogs />} />
-          <Route path="/healthcheck-log" exact element={<HealthcheckLogs />} />
+          <Routes>
+            <Route path="/dashboard" exact element={<Dashboard />} />
+            <Route path="/access-log" exact element={<AccessLogs />} />
+            <Route
+              path="/healthcheck-log"
+              exact
+              element={<HealthcheckLogs />}
+            />
 
-          <Route path="/lock" exact element={<Locks />} />
-          <Route path="/lock/edit/:lockId" exact element={<EditLock />} />
+            <Route path="/lock" exact element={<Locks />} />
+            <Route path="/lock/edit/:lockId" exact element={<EditLock />} />
 
-          <Route path="/personel" exact element={<Personels />} />
-          <Route path="/personel/add" exact element={<AddPersonel />} />
-          <Route path="/personel/edit/:personelId" element={<EditPersonel />} />
+            <Route path="/personel" exact element={<Personels />} />
+            <Route path="/personel/add" exact element={<AddPersonel />} />
+            <Route
+              path="/personel/edit/:personelId"
+              element={<EditPersonel />}
+            />
 
-          <Route path="/key" exact element={<Keys />} />
-          <Route path="/key/add" exact element={<AddKey />} />
-          <Route path="/key/edit/:keyId" exact element={<EditKey />} />
-          <Route path="/key/position" exact element={<KeyPosition />} />
-        </Routes>
+            <Route path="/key" exact element={<Keys />} />
+            <Route path="/key/add" exact element={<AddKey />} />
+            <Route path="/key/edit/:keyId" exact element={<EditKey />} />
+            <Route path="/key/position" exact element={<KeyPosition />} />
+          </Routes>
+        </Suspense>
       </Router>
     </ThemeProvider>
     // <div>
--- src/components/LoaderCover.jsx
+++ src/components/LoaderCover.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 
-const LoaderCover = ({ show }) => {
+const LoaderCover = ({ show, i = 0 }) => {
   return (
     <Backdrop
-      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      style={{ position: "absolute", height: "100%", width: "100%" }}
+      sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 + i }}
       open={show}
     >
       <CircularProgress color="inherit" size={80} />
