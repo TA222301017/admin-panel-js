fc3de0f76cb02197e0fed60ee092ba7e633cba90
Desktop Taruna <muhamad.taruna@outlook.co.id>
2023-01-22 09:26:33 +0700

    Update Edit Pages {EditLock ; EditPersonel{
    Edit Personel belum nambahin Card input buat access point yang bolh dilalui

--- src/pages/EditLock.jsx
+++ src/pages/EditLock.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import LoggedInLayout from "../layouts/LoggedInLayout";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { fontSize } from "@mui/system";
 
 const crumbs = [
   {
@@ -17,13 +21,87 @@ const crumbs = [
 ];
 
 const EditLock = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      Location: data.get("Location"),
+      IPAddress: data.get("IPAddress"),
+      LockID: data.get("LockID"),
+      PublicKey: data.get("PublicKey"),
+      Keterangan: data.get("Keterangan"),
+    });
+  };
+
   return (
     <LoggedInLayout
       title="Edit Lock"
       desc="Ubah data lock yang ada dalam sistem"
       breadcrumbs={crumbs}
     >
-      <div>UNIMPLEMENTED</div>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          "& .MuiTextField-root": { m: 2, width: "50ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <div>
+            <TextField
+              required
+              id="Location"
+              name="Location"
+              label="Location Keys"
+              helperText="Berisikan Lokasi Kunci"
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="IPAddress"
+              name="IPAddress"
+              label="IP Address"
+              helperText="Berisikan IP Address Kunci Terpasang"
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="LockID"
+              name="LockID"
+              label="Lock ID"
+              helperText="Berisikan ID Kunci"
+            />
+          </div>
+          <div>
+            <TextField
+              required
+              id="PublicKey"
+              name="PublicKey"
+              label="PublicKey"
+              helperText="Berisikan Public Key Kunci"
+            />
+          </div>
+          <div>
+            <TextField
+              id="Keterangan"
+              name="Keterangan"
+              label="Keterangan lebih lanjut"
+            />
+          </div>
+        </div>
+        <Button
+          type="submit"
+          variant="outlined"
+          color="inherit"
+          sx={{ m: 2, width: "57ch" }}
+        >
+          SIMPAN
+        </Button>
+      </Box>
     </LoggedInLayout>
   );
 };
